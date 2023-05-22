dcf66d1f9cdb5fa72952501311528a2322e85d99
wagnerSfarias <wagnner@universalhost.com.br>
2023-05-22 15:35:48 -0300

    update btn-back

--- src/components/BannerHome/index.js
+++ src/components/BannerHome/index.js
@@ -8,12 +8,10 @@ import { FaArrowLeft } from 'react-icons/fa';
 export default function BannerHome({ data }) {
     return (
         <div className='header' style={{
-            backgroundSize: 'cover',
             backgroundImage: `linear-gradient(${data?.backdrop_path ? `to bottom, rgba(210, 210, 219, 0.3), rgba(210, 210, 219, 0.6)),url("${baseImg}${data?.backdrop_path}"` : "Another Image Link"})`,
-            backgroundPosition: 'to center'
         }}>
             <Link to={'/'} className='btn-left'>
-                <FaArrowLeft className='svg-save' />
+                <FaArrowLeft />
             </Link>
         </div>
 
