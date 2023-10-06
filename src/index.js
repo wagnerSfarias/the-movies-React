a073773334919957977ff11f441f82c3a718c21d
wagnerSfarias <wagner@universalhost.com.br>
2023-10-06 12:17:46 -0300

    config globais styled

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import GlobalStyles from './styles/globalStyles'
+import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+  <>
+     <ToastContainer autoClose={3000} position="top-center"/>
+          <Routes />
+     <GlobalStyles/>
+  </>
 );
 
